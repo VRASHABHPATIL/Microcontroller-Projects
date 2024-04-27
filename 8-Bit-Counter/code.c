50902d90fed19e4ed36f7617aae93a082447c6e3
VRASHABHPATIL <vrashabhpatil047@gmail.com>
2024-04-27 17:06:19 +0100

    Update code.c

--- 8-Bit-Counter/code.c
+++ 8-Bit-Counter/code.c
@@ -1,1 +1,17 @@
-
+#include <avr/io.h>
+#include <util/delay.h>
+#define LED_PORT PORTC 
+#define LED_DDR DDRC 
+void displayBinary(uint8_t value) {
+    LED_PORT = value; 
+    _delay_ms(1000);
+}
+int main(void) {
+    LED_DDR = 0xFF; 
+    while (1) {
+         for (uint8_t i = 0; i <= 255; i++) {
+             displayBinary(i);
+        }
+    }
+     return 0;
+}
